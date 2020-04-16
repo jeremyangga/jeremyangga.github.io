b8fc09ff7a5d0c213a75261fd4b584dbac2d4183
jeremyangga <62837186+jeremyangga@users.noreply.github.com>
2020-04-16 11:53:27 +0700

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -54,12 +54,4 @@ button.addEventListener('click', function() {
   animate();
 });
 
-var deferredPrompt;
-window.addEventListener('beforeinstallprompt', function(event) {
-  console.log('beforeinstallprompt fired');
-  event.preventDefault();
-  deferredPrompt = event;
-  return false;
-});
-
 
