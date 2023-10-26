21a800c3ea2f9ee7af52e089d6037e6b95f5d279
sawzse <affrianr11@gmail.com>
2023-10-27 06:07:40 +0700

    .

---
+++ cart.js
@@ -0,0 +1,110 @@
+// const userBelanja = JSON.parse(localStorage.getItem('outputBelanja'));
+// // console.log(Object.keys(userBelanja).length);
+// console.log(userBelanja);
+// window.localStorage.setItem("outputBelanja", JSON.stringify(userBelanja));
+function getDataFromLocalStorage() {
+    let orders = JSON.parse(window.localStorage.getItem('outputBelanja'));
+  
+    if (!orders) {
+      orders = {};
+    }
+  
+    return orders;
+}
+
+let userBelanja = getDataFromLocalStorage();
+let hargaTotal = 0;
+function showCart(){
+    let html = "";
+    let checkout = "";
+    if(Object.keys(userBelanja).length === 0 || !userBelanja){
+        
+        html = "<h1>Tidak ada data!</h1> <h2>Silahkan melakukan pembelian dahulu</h2>"
+        console.log('masuk');
+    } else {
+        for(let keys in userBelanja){
+            let hargaInRupiah = new Intl.NumberFormat("id-ID", { style: "currency", currency:"IDR"}).format(userBelanja[keys]['harga']);
+            console.log(keys);
+            hargaTotal += userBelanja[keys]['harga'];
+            html += `<div class="order-header">
+            <div id="tmp-order" class="order-item">
+            <h3 id="nama-laptop" class="capitalize">${userBelanja[keys]['nama']}</h3>
+          </div> <div class="item-laptop">
+          <div> <img id="gambar-laptop" class="laptop" src="${userBelanja[keys]['gambar']}"> </div>
+          <div class="column-2">
+            <div class="row-2">
+            <input type="number" id="quantity${keys}" onchange="controlStock(${keys}, ${userBelanja[keys]['harga']})" name="quantity" min="1" max="${userBelanja[keys]["stok"]}" value="1">
+                <div>Stock: <span id="stock${keys}">${userBelanja[keys]["stok"]}</span></div>
+            </div>
+            <div class="row-3">
+              <div id="harga">Harga:<p id='hargaLaptop${keys}'>${hargaInRupiah}</p></div>
+            </div>
+            <button id="deleteBarang" onclick="hapusBarang(${keys})">Hapus</button>
+            </div>
+           </div>
+           </div>
+          `
+        }
+        checkout = `
+            <div id="checkout-info">
+                <span>Total Belanja</span>
+                <span id="checkout-harga">${new Intl.NumberFormat("id-ID", { style: "currency", currency:"IDR"}).format(hargaTotal)}</span>
+            </div>
+            <div class="checkout-btn">
+                <a href="thankyou.html">Checkout</a>
+            </div>
+            `
+    
+    }
+    
+    document.querySelector('.order-list').innerHTML = html
+    document.querySelector('.checkout-container').innerHTML = checkout
+}
+
+function hapusBarang(id) { 
+    delete userBelanja[id]
+    showCart()
+}
+
+
+let hargaIncludeStock = 0;
+function controlStock(id, harga){
+//     let x = document.getElementById("harga");
+//   x.value = x.value.toUpperCase();
+    console.log(id,'--');
+    let stockUpdate = document.getElementById(`quantity${id}`).value;
+    let hargaUpdate = harga * Number(stockUpdate);
+    let hargaInRupiah = new Intl.NumberFormat("id-ID", { style: "currency", currency:"IDR"}).format(hargaUpdate);
+    console.log(hargaInRupiah);
+    document.getElementById(`hargaLaptop${id}`).innerHTML = hargaInRupiah;
+    let stockInHTML = document.getElementById(`stock${id}`).textContent;
+    console.log(stockInHTML);
+    let stockNow = Number(stockInHTML) - Number(stockUpdate);
+    console.log(stockNow);
+    // document.getElementById(`stock${id}`).innerHTML = stockNow;
+    if(Number(stockUpdate) > 1){
+        hargaIncludeStock += hargaTotal + hargaUpdate;
+        console.log(hargaIncludeStock);
+        hargaLaptopTotal(hargaIncludeStock);
+    } else{
+
+    }
+}
+
+
+showCart();
+console.log(hargaTotal,'harga');
+
+function hargaLaptopTotal(harga){
+    let html = '';
+    html = `
+    <div id="checkout-info">
+    <span>Total Belanja</span>
+    <span id="checkout-harga">${new Intl.NumberFormat("id-ID", { style: "currency", currency:"IDR"}).format(harga)}</span>
+    </div>
+    <div class="checkout-btn">
+    <a href="thankyou.html">Checkout</a>
+    </div>
+    `
+    document.querySelector('.checkout-container').innerHTML = html
+}
---
+++ script.js
@@ -0,0 +1,160 @@
+let dataLaptop = laptopObj;
+let keysDataLaptop = Object.keys(dataLaptop).length;
+let merekLaptop = '';
+
+// $.merekJquery = function() {
+//     alert("jQuery");
+//  };
+
+
+function contoh() {
+    console.log('contoh')
+}
+
+$(document).ready(function() {
+    $(window).on('load', function() {
+        showMerek();
+        showProduct();
+        toKeranjang();
+    });
+
+    $("#filter_merek").change(function() {
+        let selectedMerek = $(this).val();
+        console.log("Selected option value (using ID): " + selectedMerek);
+        getFilter(selectedMerek);   
+      });
+  });
+  
+function getFilter(merek){
+    console.log('getFilter()')
+    merekLaptop = merek;
+    console.log('merek laptop:', merekLaptop);
+    filterProductByMerek(merekLaptop);
+}
+
+
+function filterProductByMerek(merek){
+    console.log('filterProductByMerek()')
+
+    let template = '';
+    if(!merek){
+        return 'false';
+    } else if(merek === 'semua'){
+        showProduct();
+    }else {
+        for(let i = 0; i < dataLaptop[merek].length; i++){
+            let hargaInRupiah = new Intl.NumberFormat("id-ID", { style: "currency", currency:"IDR"}).format(dataLaptop[merek][i].harga);
+            template += `
+            <div class="card">
+                <img src="${dataLaptop[merek][i]['url']}" width="200px" height="200px">
+                <h5>${merek}</h5>
+                <h4>${dataLaptop[merek][i]['nama_laptop']}</h4>
+                <p>${hargaInRupiah}</p>
+                <p>${dataLaptop[merek][i].tipe}</p>
+                <button class="btnCart" id="btn${dataLaptop[merek][i]['id']}" onclick="toKeranjang(${dataLaptop[merek][i]['id']})" value="${dataLaptop[merek][i]['id']}">Beli Produk</button>
+            </div>
+        `
+        }
+        document.querySelector('.c-card').innerHTML = template
+    }
+    
+}
+
+function showProduct(){
+    console.log('showProduct()');
+    if(keysDataLaptop === 0){
+        document.querySelector('.c-card').innerHTML = "<h4>Data Not Found</h4>"
+    }else{
+        console.log('masuk else di produk');
+        let template = '';
+        
+        for (let keys in dataLaptop) {
+            console.log(dataLaptop[keys], "---");    
+            for(let i = 0; i < dataLaptop[keys].length; i++){
+                
+                let hargaInRupiah = new Intl.NumberFormat("id-ID", { style: "currency", currency:"IDR"}).format(dataLaptop[keys][i].harga);
+                console.log(hargaInRupiah)
+                template += `<div class="card">
+                    <img src="${dataLaptop[keys][i]['url']}" width="200px" height="200px">
+                    <h5>${keys}</h5>
+                    <h4>${dataLaptop[keys][i]['nama_laptop']}</h4>
+                    <p>${hargaInRupiah}</p>
+                    <p>${dataLaptop[keys][i].tipe}</p>
+                 <button class="btnCart" id="btn${dataLaptop[keys][i]['id']}" onclick="toKeranjang(${dataLaptop[keys][i]['id']})" value="${dataLaptop[keys][i]['id']}">Beli Produk</button>
+                </div>
+            `
+            }
+        }
+        // console.dir(document.querySelector('.c-card').innerHTML, '\n<<-- before');
+        console.log('before')
+        document.querySelector('.c-card').innerHTML = template
+        // console.dir(document.querySelector('.c-card').innerHTML);
+
+    }
+}
+
+function showMerek(){
+    console.log('showMerek()')
+
+    let filterMerek = `<option selected disabled>Filter berdasarkan merek</option>
+    <option value="semua">Semua merek</option>`;
+    for(let keys in dataLaptop){
+        filterMerek += `
+        <option value="${keys}">${keys}</option>`
+    }
+    document.querySelector('#filter_merek').innerHTML = filterMerek;
+}
+
+let idToKeranjang = 0;
+function toKeranjang(id){
+    // console.log('toKeranjang()')
+    // // let id = document.getElementsByClassName('btnCart');
+    idToKeranjang = id;
+    console.log(idToKeranjang);
+} 
+
+function getDataFromLocalStorage() {
+    let orders = JSON.parse(window.localStorage.getItem('outputBelanja'));
+  
+    if (!orders) {
+      orders = {};
+    }
+  
+    return orders;
+  }
+
+let listBelanja = getDataFromLocalStorage();
+let outputBelanja = []
+
+function toKeranjang(id){
+    // idToKeranjang = id;
+    console.log(idToKeranjang, "<<<<");
+    // console.log(dataLaptop["Asus"][id])
+    for(let keys in dataLaptop){
+        console.log(keys);
+        // console.log(dataLaptop[keys])
+        for(let i = 0; i < dataLaptop[keys].length; i++){
+            // console.log(dataLaptop[keys][i]["nama_laptop"])
+            if(dataLaptop[keys][i]["id"] === id) {
+                if(!listBelanja[id]){
+                    listBelanja[id] = {
+                        merk: keys,
+                        nama: dataLaptop[keys][i]["nama_laptop"],
+                        harga: dataLaptop[keys][i]["harga"],
+                        stok: dataLaptop[keys][i]["stok"],
+                        gambar: dataLaptop[keys][i]["url"] 
+                    }
+                }
+                // outputBelanja.push(listBelanja)
+                // console.log(listBelanja);
+            }
+        } 
+    }
+    // console.log(outputBelanja, "-----------------")
+    console.dir(listBelanja, "<<<<<<<<<<<<<<")
+    window.localStorage.setItem("outputBelanja", JSON.stringify(listBelanja));
+} 
+
+
+// showMerek();
+// showProduct();
